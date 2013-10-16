569c040e50e7fc8f29aabed234fd7db110609239
Josh <josh@toolofna.com>
2013-10-15 20:12:51 -0700

    moving stuff around

--- site/app/templates/jade_tmpls.js
+++
@@ -1,6 +0,0 @@
-this["JST"] = this["JST"] || {};
-
-this["JST"]["app/templates/main"] = function anonymous(locals) {
-var buf = [];
-var locals_ = (locals || {}),pageTitle = locals_.pageTitle;buf.push("<html lang=\"en\"><head><title>" + (jade.escape(null == (jade.interp = pageTitle) ? "" : jade.interp)) + "</title><script type=\"text/javascript\">if (foo) {\n   bar(1 + 5)\n}</script></head><body><h1>Jade - node template engine</h1></body></html>");;return buf.join("");
-};
